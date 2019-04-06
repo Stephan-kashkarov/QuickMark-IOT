f1380728dd8d939d2069102dd807053a10029bb8
Stephan-kashkarov <stephankashkarovofficial@gmail.com>
2019-04-06 19:12:52 +1100

    initail code

---
+++ src/main.cpp
@@ -0,0 +1,21 @@
+#include <Arduino.h>
+
+bool connect_wifi() {
+	return false;
+}
+
+bool connect_server(){
+	return false;
+}
+
+bool send_rfid(){
+	return false;
+}
+
+void setup() {
+	// put your setup code here, to run once:
+}
+
+void loop() {
+	// put your main code here, to run repeatedly:
+}
